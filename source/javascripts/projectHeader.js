08b7704270d997ee0bc14c0b1da0349791efebe5
amplerob <rob@helloample.com>
2016-03-18 07:38:50 -0700

    Sloan digital, Hickory docks

--- source/javascripts/projectHeader.js
+++ source/javascripts/projectHeader.js
@@ -2,7 +2,7 @@ var projectHeader = function () {
   jQuery("#responsive_headline").fitText(1);
 
   var height = document.getElementById("project-header").offsetHeight;
-  document.getElementById("project-content").style.marginTop = (height + 10) + 'px';
+  document.getElementById("project-content").style.marginTop = (height + 65) + 'px';
 };
 
 $(document).ready(projectHeader);
