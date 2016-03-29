8af1d4a33da3e33512f4e836c6b08a46f78a1384
amplerob <rob@helloample.com>
2016-03-29 15:05:52 -0700

    Revamp project headers

--- source/javascripts/projectHeader.js
+++ source/javascripts/projectHeader.js
@@ -1,5 +1,5 @@
 var projectHeader = function () {
-  jQuery("#responsive_headline").fitText(1);
+  jQuery("#responsive_headline").fitText(1.1);
 
   var height = document.getElementById("project-header").offsetHeight;
   document.getElementById("project-content").style.marginTop = (height + 65) + 'px';
