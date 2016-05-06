8c2ea50d9d3e4495e2faa08bfdaf690c6a1dd058
amplerob <rob@helloample.com>
2016-05-06 14:50:55 -0700

    Remove blog

--- config.rb
+++ config.rb
@@ -1,13 +1,10 @@
-activate :blog do |blog|
-  blog.prefix = "press/"
-  blog.layout = "blog"
-  blog.summary_length = 200
-  blog.paginate = true
-  blog.per_page = 6
-  blog.page_link = "page/{num}"
-  blog.tag_template = "press/tag.html"
-  blog.taglink = "/{tag}.html"
-end
+# activate :blog do |blog|
+#   blog.prefix = "journal/"
+#   blog.layout = "blog"
+#   blog.summary_length = 200
+#   blog.paginate = true
+#   blog.per_page = 6
+# end
 
 set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
