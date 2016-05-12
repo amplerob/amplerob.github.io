dd6b19883fd20e66884642b986d4baf89bb9a238
amplerob <rob@helloample.com>
2016-05-12 08:10:52 -0700

    Tweak mobile view

--- source/javascripts/projectHeader.js
+++ source/javascripts/projectHeader.js
@@ -1,5 +1,19 @@
+// var projectHeader = function () {
+//   jQuery("#responsive_headline").fitText(1.1);
+//
+//   var height = document.getElementById("project-header").offsetHeight;
+//   document.getElementById("project-content").style.marginTop = (height + 65) + 'px';
+// };
+
+
+
+
 var projectHeader = function () {
-  jQuery("#responsive_headline").fitText(1.1);
+  if (document.documentElement.clientWidth > 767) {
+    jQuery("#responsive_headline").fitText(1.1);
+  } else {
+    jQuery("#responsive_headline").fitText(.6);
+  }
 
   var height = document.getElementById("project-header").offsetHeight;
   document.getElementById("project-content").style.marginTop = (height + 65) + 'px';
