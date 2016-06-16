ee7471b743fef9d49460733c45a590e3d7f2c47f
amplerob <rob@helloample.com>
2016-06-16 12:36:00 -0700

    HTML 5 semantics, accessibility improvements, descriptions for Eagle and Bakersfield

--- source/javascripts/projectHeader.js
+++ source/javascripts/projectHeader.js
@@ -5,9 +5,6 @@
 //   document.getElementById("project-content").style.marginTop = (height + 65) + 'px';
 // };
 
-
-
-
 var projectHeader = function () {
   if (document.documentElement.clientWidth > 767) {
     jQuery("#responsive_headline").fitText(1.1);
